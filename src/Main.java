4db512992be6cbb5b96666a665cd6f027d2cfb10
dawidchec <checdd@gmail.com>
2022-11-14 15:51:57 +0100

    Creating LabelfLogIn

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,26 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Main {
     public static void main(String[] args) {
 
-        OpenZalando();
+        //OpenZalando();
         //OpenYouTube();
         //SearchGmail();
+        LabelfLogIn();
 
+    }
+
+    public static void LabelfLogIn(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://stag.labelf.ai/login");
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        WebElement el = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"diffuse-cookie-notice\"]/div/div/div/div[2]/button[3]")));
+        el.click();
+        driver.findElement(By.name("login")).sendKeys("checdd@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("kufowo92");
+        driver.findElement(By.className("v-btn__content")).click();
+
+        //driver.quit();
 
     }
 
