e8c506e59020098fbddf50fde15f0cbe50ffad65
DawidChec <checdd@gmail.com>
2022-11-19 01:11:24 +0100

    Adding Helper files

--- src/Helpers/DriverSetup.java
+++ src/Helpers/DriverSetup.java
@@ -1,16 +1,11 @@
 package Helpers;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
--- src/Helpers/SeleniumHelper.java
+++ src/Helpers/SeleniumHelper.java
@@ -1,9 +1,7 @@
 package Helpers;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
