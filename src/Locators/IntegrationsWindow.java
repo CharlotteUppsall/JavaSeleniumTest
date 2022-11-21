978242ffa970f0dbf89791ab7ca9406ee4a40b5d
dawidchec <checdd@gmail.com>
2022-11-21 13:35:13 +0100

    Adding Integrationsfile

--- src/Locators/IntegrationsWindow.java
+++ src/Locators/IntegrationsWindow.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 public class IntegrationsWindow {
     public static By CreateMyIntegrtion = By.name("My Integrations");
     public static By CreateAPIToken = By.name(" Create API-token");
+    public static By ConnectZendeskWithLabelf = By.xpath("//*[@id=\"app\"]/div[5]/div[1]/main/div/div/ul/li[2]/div[2]/div[1]/div/div/div/div/div[2]/div[2]/a/div/i");
     public static By ConnectZendeswithLabelf = By.name(" 2. Connect Zendesk with Labelf ");
     public static By FreshServiceButton = By.xpath("//*[@id=\"app\"]/div[5]/div[1]/main/div/div/ul/li[2]/div[2]/div[3]/div/div/div/div/div[2]/div/a/div");
     public static By FreshDeskButton = By.xpath("//*[@id=\"app\"]/div[5]/div[1]/main/div/div/ul/li[2]/div[2]/div[4]/div/div/div/div/div[2]/div/a/div/i");
