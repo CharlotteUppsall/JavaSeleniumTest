24e7db35990aa913fdd33fde6fb4e2c9f35508d0
DawidChec <checdd@gmail.com>
2022-11-26 12:22:07 +0100

    StartTrainingPage

--- src/Locators/LoginWindow.java
+++ src/Locators/LoginWindow.java
@@ -2,7 +2,7 @@ package Locators;
 
 import org.openqa.selenium.By;
 
-public class LoginWindowLocators {
+public class LoginWindow {
     public static By acceptCookies = By.xpath("//*[@id=\"diffuse-cookie-notice\"]/div/div/div/div[2]/button[3]");
     public static By usernameBox = By.name("login");
     public static By passwordBox =  By.name("password");
--- src/Locators/SartTrainingPage.java
+++ src/Locators/SartTrainingPage.java
@@ -4,4 +4,8 @@ import org.openqa.selenium.By;
 
 public class SartTrainingPage {
     public static By SettingOnStartTrainingPage =By.xpath("//*[contains(text(),'settings')]");
+    public static By OveriewOnStartTrainingPage =By.xpath("//*[contains(text(),'Overview')]");
+    public static By APIOnStartTrainingPage =By.xpath("//*[contains(text(),'API')]");
+    public static By StartTrainingOnStartTrainingPage =By.xpath("//*[contains(text(),'Start training')]");
+    public static By MetricsOnStartTrainingPage =By.xpath("//*[contains(text(),'Metrics')]");
 }
